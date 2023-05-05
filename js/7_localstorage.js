5c74063bfb00f8c1627a28776685b54a215dc8e1
mazielbook <valeriamaziel@gmail.com>
2023-05-05 17:13:06 -0400

    actualizacion

---
+++ js/7_localstorage.js
@@ -0,0 +1,12 @@
+localStorage.setItem('valele', 'luna')
+
+const datos = [{ 
+    nombre:'valeria'
+},{ nombre:'enrique'
+}
+     ]
+
+     localStorage.setItem('datos', JSON.stringify(datos))
+     console.log(responde)
+
+
---
+++ js/8_DOM.js
@@ -0,0 +1,18 @@
+const container = document.getElementById('container')
+// const nuevoElemento = document.createElement('button')
+
+const pintar = async () => { } 
+
+
+
+
+
+
+// console.log(container)
+
+// const data = [1,2]
+
+// container.classList.add('flex', 'flex-col')
+
+// const funcion = (data) => { data.map () } 
+// container.innerText = '
